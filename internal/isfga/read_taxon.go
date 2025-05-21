284c79748cb6072362c412175b30357cd36446ba
Dmitry Mozzherin <dmozzherin@gmail.com>
2025-05-21 16:41:28 +0000

    read flat hierarchy IDs (close #15)

--- internal/isfga/read_taxon.go
+++ internal/isfga/read_taxon.go
@@ -21,11 +21,14 @@ SELECT
 	col__according_to_page, col__according_to_page_link, col__scrutinizer,
 	col__scrutinizer_id, col__status_id, col__extinct,
 	col__temporal_range_start_id, col__temporal_range_end_id,
-	col__environment_id, col__species, col__section, col__subgenus, col__genus,
-	col__subtribe, col__tribe, col__subfamily, col__family, col__superfamily,
-	col__suborder, col__order, col__subclass, col__class, col__subphylum,
-	col__phylum, col__kingdom, col__reference_id, col__link, col__remarks,
-	col__modified, col__modified_by
+	col__environment_id, col__species, sf__species_id, col__section,
+	sf__section_id, col__subgenus, sf__subgenus_id, col__genus, sf__genus_id,
+	col__subtribe, sf__subtribe_id, col__tribe, sf__tribe_id, col__subfamily,
+	sf__family_id, col__family, sf__family_id, col__superfamily,
+	sf__superfamily_id, col__suborder, sf__suborder_id, col__order, sf__order_id,
+	col__subclass, sf__subclass_id, col__class, sf__class_id, col__subphylum,
+	sf__subphylum_id, col__phylum, sf__phylum_id, col__kingdom, sf__kingdom_id,
+	col__reference_id, col__link, col__remarks, col__modified, col__modified_by
 FROM taxon
 `
 	rows, err := a.db.QueryContext(ctx, q)
@@ -45,10 +48,14 @@ FROM taxon
 			&tx.BranchLength, &tx.NameID, &tx.NamePhrase, &tx.AccordingToID,
 			&tx.AccordingToPage, &tx.AccordingToPageLink, &tx.Scrutinizer,
 			&tx.ScrutinizerID, &status, &tx.Extinct, &start, &end, &env, &tx.Species,
-			&tx.Section, &tx.Subgenus, &tx.Genus, &tx.Subtribe, &tx.Tribe,
-			&tx.Subfamily, &tx.Family, &tx.Superfamily, &tx.Suborder, &tx.Order,
-			&tx.Subclass, &tx.Class, &tx.Subphylum, &tx.Phylum, &tx.Kingdom,
-			&tx.ReferenceID, &tx.Link, &tx.Remarks, &tx.Modified, &tx.ModifiedBy,
+			&tx.SpeciesID, &tx.Section, &tx.SectionID, &tx.Subgenus, &tx.SubgenusID,
+			&tx.Genus, &tx.GenusID, &tx.Subtribe, &tx.SubtribeID, &tx.Tribe,
+			&tx.TribeID, &tx.Subfamily, &tx.SubfamilyID, &tx.Family, &tx.FamilyID,
+			&tx.Superfamily, &tx.SuperfamilyID, &tx.Suborder, &tx.SuborderID,
+			&tx.Order, &tx.OrderID, &tx.Subclass, &tx.SubclassID, &tx.Class,
+			&tx.ClassID, &tx.Subphylum, &tx.SubphylumID, &tx.Phylum, &tx.PhylumID,
+			&tx.Kingdom, &tx.KingdomID, &tx.ReferenceID, &tx.Link, &tx.Remarks,
+			&tx.Modified, &tx.ModifiedBy,
 		)
 		if err != nil {
 			return fmt.Errorf("cannot scan author row: %w", err)
